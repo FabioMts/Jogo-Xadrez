d11750837dece2c1259bcf412463f4f0d9543af6
Fabio Matos <fabio.thunder@hotmail.com>
2021-11-02 15:53:16 -0300

    começando a implementar o tabuleiro

---
+++ tabuleiro/Peca.cs
@@ -0,0 +1,14 @@
+﻿
+using tabuleiro;
+
+namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qteMovimentos { get; protected set; }
+        public Tabuleiro tab { get; set; }
+
+    }
+}
---
+++ tabuleiro/Tabuleiro.cs
@@ -0,0 +1,7 @@
+﻿
+namespace tabuleiro
+{
+    class Tabuleiro
+    {
+    }
+}
