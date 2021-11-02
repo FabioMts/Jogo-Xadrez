09d74e2cdfab655db395954d86ed74978cd615fc
Fabio Matos <fabio.thunder@hotmail.com>
2021-11-01 23:46:25 -0300

    criação primeira classe

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using tabuleiro;
 
 namespace xadrez_console
 {
@@ -6,7 +7,8 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao p = new Posicao(3, 4);
+            Console.WriteLine("Posicao: " + p);
         }
     }
 }
---
+++ tabuleiro/Posicao.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabuleiro
+{
+    class Posicao
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Posicao()
+        {
+
+        }
+
+        public Posicao(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return Linha
+                + ", "
+                + Coluna;
+        }
+    }
+}
