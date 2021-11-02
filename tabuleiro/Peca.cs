05a295bf92f620e8967c1cff1a8fc037e8e4995e
Fabio Matos <fabio.thunder@hotmail.com>
2021-11-02 16:45:03 -0300

    começando a colocar pecas no tabuleiro

--- tabuleiro/Peca.cs
+++ tabuleiro/Peca.cs
@@ -10,9 +10,9 @@ namespace tabuleiro
         public int qteMovimentos { get; protected set; }
         public Tabuleiro tab { get; set; }
 
-        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        public Peca(Tabuleiro tab, Cor cor)
         {
-            this.posicao = posicao;
+            this.posicao = null;
             this.cor = cor;
             this.qteMovimentos = 0;
             this.tab = tab;
